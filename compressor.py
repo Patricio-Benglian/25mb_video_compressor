c7e6c0248836e87a966faef1a728b7a31244215f
Patricio-Benglian <6305@holbertonstudents.com>
2023-12-12 11:09:09 -0300

    Changing env python3 to env python fixed the not finding ffmpeg module error.

--- compressor.py
+++ compressor.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import ffmpeg
 import os
 import tkinter as tk
 from tkinter import filedialog
 
-
+# From what I understand, this creates the 'parent' window
+# Other windows (filedialog) become child windows of it
 root = tk.Tk()
-root.withdraw()
+root.withdraw()  # Commenting this out will open filedialog AND the root window
 
 file_path = filedialog.askopenfilename()
 
@@ -15,6 +16,8 @@ def compress(file_path):
   print(f"Filepath is {file_path}")
 
 compress(file_path)
+
+
 
 
 
