86590efe686b23f4a01cbd7125fbc3209573139b
Patricio-Benglian <6305@holbertonstudents.com>
2023-12-14 15:25:01 -0300

    Seems to be working. IDK if the codecs chosen are the best for the job though.

--- compressor.py
+++ compressor.py
@@ -1,30 +1,77 @@
 #!/usr/bin/env python
 
+from filepath_func import find_file, find_save_directory
 import ffmpeg
 import os
-from tkinter import filedialog
-
-def find_file():
-  file_path = filedialog.askopenfilename()
-  if file_path:
-    print(f"Filepath is {file_path}")
-  return file_path
 
 def compress(file_path, target_filesize=25):
-  pass
+  file_info = ffmpeg.probe(file_path)
 
-def find_save_directory():
-  save_directory = filedialog.askdirectory()
-  if save_directory:
-    print(f"Saving to {save_directory}")
-  return save_directory
+  # Get duration (mult. by bitrate to get filesize)
+  duration = float(file_info['format']['duration'])
 
-file_path = find_file()
-compress(file_path)
-save_directory = find_save_directory()
+  print(f"Duration: {duration}")
 
+  # Get bitrates
+  _video_bitrate = int(file_info['streams'][0]['bit_rate'])
+  _audio_bitrate = int(file_info['streams'][1]['bit_rate'])
+  total_bitrate = _audio_bitrate + _video_bitrate
 
+  print(f"Total Bitrate: {total_bitrate}\n\
+    Video Bitrate: {_video_bitrate}\n\
+    Audio Bitrate: {_audio_bitrate}")
 
+  # Calculate bitrate ratios
+  video_bitrate_ratio = _video_bitrate / total_bitrate
+  audio_bitrate_ratio = _audio_bitrate / total_bitrate
 
+  print(f"Video Bitratio: {video_bitrate_ratio}")
+  print(f"Audio Bitratio: {audio_bitrate_ratio}")
 
+  # Calculate desired bitrate for target filesize
+    # For future reference:
+    # *8 to go from bit to byte
+    # *1000 to go from byte to kilobyte
+    # *1000 to go from kilo to megabyte
+  target_bitrate = int(target_filesize * 8 * 1000 * 1000 / duration)
+  # Reduce it just a little so it doesn't reach target filesize exactly (Just in case?)
+  target_bitrate = int(target_bitrate * 0.99)
 
+  print(f"Target Bitrate: {target_bitrate}\n\
+    Projected Filesize: {round(target_bitrate * duration / 8 / 1000 / 1000, 2)}")
+
+  # Calculate new bitrate values for desired filesize
+  new_video_bitrate = int(target_bitrate * video_bitrate_ratio)
+  new_audio_bitrate = int(target_bitrate * audio_bitrate_ratio)
+
+  print(f"Total New Bitrate: {new_video_bitrate + new_audio_bitrate}\n\
+    New Video Bitrate: {new_video_bitrate}\n\
+    New Audio Bitrate: {new_audio_bitrate}")
+
+  # Get filename and designate name for new file
+  input_filename = os.path.basename(file_path)
+  output_filename = "compr_" + input_filename
+  print(f"Filename: {input_filename}")
+  print(f"New filename: {output_filename}")
+
+  # Get directory to save to
+  save_directory = find_save_directory()
+  if save_directory == None:
+    print("No directory selected, exiting...")
+    quit()
+  # Designate full path to the file created
+  output_file_path = os.path.join(save_directory, output_filename)
+  print(f"Saving file in: {output_file_path}")
+
+  # Run the first pass to estimate the bitrate
+  input_pass1 = ffmpeg.input(file_path)
+  output_pass = input_pass1.output(output_file_path, acodec='aac',
+    vcodec='hevc', 
+    video_bitrate=new_video_bitrate, 
+    audio_bitrate=new_audio_bitrate, 
+    format='mp4')
+  ffmpeg.run(output_pass)
+
+if __name__ == "__main__":
+  file_path = find_file()
+  compress(file_path)
