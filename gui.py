d67c4fb63b1708ae9b643649aa501091436612b8
Patricio-Benglian <6305@holbertonstudents.com>
2024-01-07 11:33:51 -0300

    UI updates. Filesize range is 8-25

--- gui.py
+++ gui.py
@@ -12,6 +12,7 @@ class Gui_Window:
   def __init__(self):
     self.file_path = None
     self.save_directory = None
+    self.slider_filesize = 25
     # Tkinter gfx init
     self.root = tk.Tk() # Create the base window
     self.root.title("Video Compressor")  # Title of the Window
@@ -20,10 +21,11 @@ class Gui_Window:
     # Create the widgets
 
     # Target Filesize Widgets
-    self.size_label = tk.Label(self.root, text="Desired Filesize (MB):")
+    self.size_label = tk.Label(self.root, text=f"Desired Filesize (MB): {self.slider_filesize}")
     self.size_label.pack(pady=10, padx=30, anchor="w")
 
-    self.size_slider = ttk.Scale(self.root, from_=8, to=500, length=200, orient="horizontal")
+    
+    self.size_slider = ttk.Scale(self.root, from_=8, to=25, length=200, orient="horizontal", command=partial(self.update_slider,))
     self.size_slider.set(25)  # Default value
     self.size_slider.pack(pady=10, padx=30, anchor="w")
 
@@ -31,19 +33,24 @@ class Gui_Window:
     self.upload_button = tk.Button(self.root, text="Upload File", command=partial(self.find_file))
     self.upload_button.pack(pady=10, padx=30, anchor="w")
 
-    self.file_label = tk.Label(self.root, text="Selected File: None")  # Needs to update with filename
+    self.file_label = tk.Label(self.root, text=f"Selected File: {self.file_path}")  # Needs to update with filename
     self.file_label.pack(pady=10, padx=30, anchor="w")
 
     # Compression Widget
-    self.compress_button = tk.Button(self.root, text="Compressor", command=partial(self.compress))
+    self.compress_button = tk.Button(self.root, text="Compress", command=partial(self.compress))
     self.compress_button.pack(pady=20, padx=30, anchor="w")
 
     # Loops (like a console)
     # Updates GUI and checks for events (widget updates)
     self.root.mainloop()
 
+  def update_slider(self, slider_value):
+    self.slider_filesize = int(self.size_slider.get())
+    self.size_label.config(text=f"Desired Filesize (MB): {self.slider_filesize}")
+
   def find_file(self):
     self.file_path = tk.filedialog.askopenfilename()
+    print (self.file_path)
     if self.file_path:
       print(f"Filepath is {self.file_path}")
     else:
@@ -53,7 +60,10 @@ class Gui_Window:
     if self.file_path[-4:] != ".mp4":
       print ("Currently only support mp4 :/")
       self.file_path = None
-    # Maybe have error text/warning if none selected
+
+    # Update the label with the selected file
+    if self.file_path is not None:
+      self.file_label.config(text=f"Selected File: {os.path.basename(self.file_path)}")
 
   def find_save_directory(self):
     self.save_directory = tk.filedialog.askdirectory()
@@ -63,12 +73,17 @@ class Gui_Window:
     else:
       print("No file selected")
 
-  def compress(self, target_filesize=25):
+  def compress(self):
+
     if self.file_path == None:
       return
+    # Turn self variables into normal variables
     file_path = self.file_path
     file_info = ffmpeg.probe(file_path)
+    target_filesize = self.slider_filesize
 
+    # Update button to indicate it's in progress
+    self.compress_button.config(text="In Progress...")
 
     # Get duration (mult. by bitrate to get filesize)
     duration = float(file_info["format"]["duration"])
@@ -99,8 +114,13 @@ class Gui_Window:
     # *1000 to go from byte to kilobyte
     # *1000 to go from kilo to megabyte
     target_bitrate = int(target_filesize * 8 * 1000 * 1000 / duration)
-    # Reduce it just a little so it doesn't reach target filesize exactly (Just in case?)
-    target_bitrate = int(target_bitrate * 0.95)
+
+    # Makes room for file overhead
+    # Uses linear interp to make more room the smaller the size (since overhead is a greater portion of total size)
+    target_bitrate = int(target_bitrate * (min(
+      0.75 + ( (0.95 - 0.8) * (target_filesize - 0.75) ) / (25 - 8), 
+      0.95)))
+
 
     print(
         f"Target Bitrate: {target_bitrate}\n\
@@ -146,6 +166,11 @@ class Gui_Window:
         format="mp4",
     )
     ffmpeg.run(output_vid)
+
+    # Reset tkinter window to defaults when finished
+    self.file_path = None
+    self.file_label.config(text=f"Selected File: {self.file_path}")
+    self.compress_button.config(text="Compress")
 
 
 if __name__ == "__main__":
